aeeb840d054f2645ca02ecd009a6fb27d5b814af
jean <jean.dumoulin@insa-rennes.fr>
2020-03-20 19:19:00 +0100

    working code: button, pwm, LED, ADC

--- Software/src/main.rs
+++ Software/src/main.rs
@@ -2,134 +2,64 @@
 
 #![no_main]
 #![no_std]
+#![allow(non_snake_case)] 
+#![deny(unsafe_code)]
+#![deny(warnings)]
 
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use stm32l0xx_hal as _;
+use stm32l0xx_hal::{self, pac, pwm, prelude::*, rcc::Config};
 
 extern crate cortex_m;
 
-// Peripheral addresses as constants
-#[rustfmt::skip]
-mod address {
-    pub const PERIPH_BASE: u32      = 0x40000000;
-    pub const AHB1PERIPH_BASE: u32  = PERIPH_BASE + 0x00020000;
-    pub const RCC_BASE: u32         = AHB1PERIPH_BASE + 0x3800;
-    pub const RCC_AHB1ENR: u32      = RCC_BASE + 0x30;
-    pub const GBPIA_BASE: u32       = AHB1PERIPH_BASE + 0x0000;
-    pub const GPIOA_MODER: u32      = GBPIA_BASE + 0x00;
-    pub const GPIOA_BSRR: u32       = GBPIA_BASE + 0x18;
-}
-
-use address::*;
-
-#[inline(always)]
-fn read_u32(addr: u32) -> u32 {
-    unsafe { core::ptr::read_volatile(addr as *const _) }
-    //core::ptr::read_volatile(addr as *const _)
-}
-
-#[inline(always)]
-fn write_u32(addr: u32, val: u32) {
-    unsafe {
-        core::ptr::write_volatile(addr as *mut _, val);
-    }
-}
-
-fn wait(i: u32) {
-    for _ in 0..i {
-        cortex_m::asm::nop(); // no operation (cannot be optimized out)
-    }
-}
-
 #[entry]
 fn main() -> ! {
-    hprintln!("Init GPIO").unwrap();
-    // power on GPIOA
-    let r = read_u32(RCC_AHB1ENR); // read
-    write_u32(RCC_AHB1ENR, r | 1); // set enable
 
-    // configure PA4 as output
-    let r = read_u32(GPIOA_MODER) & !(0b11 << (4 * 2)); // read and mask
-    write_u32(GPIOA_MODER, r | 0b01 << (4 * 2)); // set output mode
+    let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    // Configure the clock.
+    let mut rcc = dp.RCC.freeze(Config::hsi16());
+
+    // Get the delay provider.
+    let mut delay = cp.SYST.delay(rcc.clocks);
+
+    // Acquire the GPIOA peripheral. This also enables the clock for GPIOA in
+    // the RCC register.
+    let gpioa = dp.GPIOA.split(&mut rcc);
+
+    // Configure PA8 as output (info LED output)
+    let mut infoLED = gpioa.pa8.into_push_pull_output();
+
+    // Configure PA5 as input (SW2)
+    let button = gpioa.pa5.into_push_pull_output();
+    
+    // Configure the timer as PWM on PA0.
+    let pwm = pwm::Timer::new(dp.TIM2, 1.khz(), &mut rcc);
+    let mut pwm = pwm.channel1.assign(gpioa.pa0);
+    let max_duty = pwm.get_max_duty() / 4095;
+    pwm.enable();
+    
+    // Configure ADC on PA2
+    let mut adc = dp.ADC.constrain(&mut rcc);
+    let mut a3 = gpioa.pa3.into_analog();
+
+
     loop {
-        // set PA4 high
-        //write_u32(GPIOA_BSRR, 1 << 4); // set bit, output hight (turn on led)
-        continue;
-        // set PA4 low
-        //write_u32(GPIOA_BSRR, 1 << (4 + 16)); // clear bit, output low (turn off led)
-        //hprintln!("Hello, world!").unwrap();
+
+        let val: u16 = adc.read(&mut a3).unwrap();
+        if button.is_high().unwrap() {
+            infoLED.set_high().unwrap();
+            hprintln!("LED high: {}", val).unwrap();
+            delay.delay(500.ms());
+        } else {
+            hprintln!("LED low: {}", val).unwrap();
+            infoLED.set_low().unwrap();
+        }
+        //i2c.write(MAX17048_ADDR, &mut buffer).unwrap();
+
+        pwm.set_duty(max_duty * 0);
     }
 }
-
-/*// ! Prints "Hello, world!" on the host console using semihosting
-
-#![no_main]
-#![no_std]
-//#![deny(warnings)]
-//#![deny(unsafe_code)]
-
-extern crate panic_halt;
-extern crate cortex_m;
-
-use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-
-
-// Peripheral addresses as constants
-#[rustfmt::skip]
-mod address {
-    pub const PERIPH_BASE: u32      = 0x40000000;
-    pub const AHB1PERIPH_BASE: u32  = PERIPH_BASE + 0x00020000;
-    pub const RCC_BASE: u32         = AHB1PERIPH_BASE + 0x3800;
-    pub const RCC_AHB1ENR: u32      = RCC_BASE + 0x30;
-    pub const GBPIA_BASE: u32       = AHB1PERIPH_BASE + 0x0000;
-    pub const GPIOA_MODER: u32      = GBPIA_BASE + 0x00;
-    pub const GPIOA_BSRR: u32       = GBPIA_BASE + 0x18;
-}
-
-use address::*;
-
-#[inline(always)]
-fn read_u32(addr: u32) -> u32 {
-    unsafe { core::ptr::read_volatile(addr as *const _) }
-    //core::ptr::read_volatile(addr as *const _)
-}
-
-#[inline(always)]
-fn write_u32(addr: u32, val: u32) {
-    unsafe {
-        core::ptr::write_volatile(addr as *mut _, val);
-    }
-}
-
-fn wait(i: u32) {
-    for _ in 0..i {
-        cortex_m::asm::nop(); // no operation (cannot be optimized out)
-    }
-}
-
-#[entry]
-fn main() -> ! {
-    // power on GPIOA
-    let r = read_u32(RCC_AHB1ENR); // read
-    write_u32(RCC_AHB1ENR, r | 1); // set enable
-
-    // configure PA8 as output
-    let r = read_u32(GPIOA_MODER) & !(0b11 << (8 * 2)); // read and mask
-    write_u32(GPIOA_MODER, r | 0b01 << (8 * 2)); // set output mode
-
-    hprintln!("Hello, world!").unwrap();
-    loop {
-        // set PA5 high
-        write_u32(GPIOA_BSRR, 1 << 5); // set bit, output hight (turn on led)
-        wait(10_000);
-
-        // set PA5 low
-        write_u32(GPIOA_BSRR, 1 << (5 + 16)); // clear bit, output low (turn off led)
-        wait(10_000);
-    }
-}
-*/
